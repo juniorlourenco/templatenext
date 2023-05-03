1ab23dbc104bdecc915c156b7cc49cc768d4dc7a
juniorlourenco <juniorminaj101@gmail.com>
2023-05-03 12:44:18 -0400

    setting up first styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ const FooterCard = styled.div`
 export default function Footer() {
     return ( 
     <FooterCard>
-        <p>Todos os direitos reservados Junior Lourenço © 2023</p>
+        <p>Todos os direitos reservados, Junior Lourenço © 2023</p>
         <p>Endereço: Av. Castelo Branco, 2500 - Centro Sul, Várzea Gande - MT, 78125-700 </p>
     </FooterCard>)
   }
